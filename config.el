75c3f04154dd36a2bc347059e89c82c159a1cbf3
guilherme <49197151+Guilherme-Vasconcelos@users.noreply.github.com>
2021-07-19 20:59:00 -0300

    Set up `org` dir for org-agenda.

--- config.el
+++ config.el
@@ -30,6 +30,9 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
+(unless (file-exists-p org-directory)
+  (make-directory org-directory))
+(setq org-agenda-files (list org-directory))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
