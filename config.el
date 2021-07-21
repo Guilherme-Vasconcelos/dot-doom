75699b16ef82a37957e17312f83d4b347c4091a1
guilherme <49197151+Guilherme-Vasconcelos@users.noreply.github.com>
2021-07-21 20:56:52 -0300

    Add more semicolons

--- config.el
+++ config.el
@@ -32,36 +32,36 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-;;; Themes and appearance
+;;;; Themes and appearance
 (setq doom-theme 'doom-nord)
 (setq centaur-tabs-show-navigation-buttons t
       centaur-tabs-cycle-scope 'default)
 
-;;; Directories and files
+;;;; Directories and files
 (setq org-directory "~/org/")
 (unless (file-exists-p org-directory)
   (make-directory org-directory))
 (setq org-agenda-files (list org-directory))
 
-;;; Keybinds
-; FIXME: doom-leader-alt-key is not working here.
+;;;; Keybinds
+;; FIXME: doom-leader-alt-key is not working here.
 (defhydra hydra-centaur-tabs (global-map "C-c" :color red)
   "Change centaur-tab"
   ("<left>" centaur-tabs-backward "←")
   ("<right>" centaur-tabs-forward "→"))
 
-;;; Programming languages
-; Activate TS support
+;;;; Programming languages
+;; Activate TS support
 (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
 (add-to-list 'auto-mode-alist '("\\.tsx\\'" . typescript-mode))
 
-;; Autoformat (must be enabled for each mode individually for more control).
-;; See more: https://github.com/hlissner/doom-emacs/tree/develop/modules/editor/format
-; Python
+;;; Autoformat (must be enabled for each mode individually for more control).
+;;; See more: https://github.com/hlissner/doom-emacs/tree/develop/modules/editor/format
+;; Python
 (add-hook 'python-mode-hook #'format-all-mode)
 (add-hook 'before-save-hook #'py-isort-before-save)
 
-;;; Misc variables
+;;;; Misc variables
 (setq confirm-kill-emacs nil)  ; Remove exit confirmation
 (setq display-line-numbers-type 'relative)  ; Display line numbers. Replace 'relative with t if using absolute lines.
 (setq kill-whole-line t)  ; Kill the entire line when using C-k at the beginning
